d59d6b559aafd36c7da0883b102197ae336e1fd0
izaeleffemberg <izaelcosta@icloud.com>
2019-07-03 09:38:10 -0300

    Implemnt the search method via DFS algorithm

--- Trie/AddAndSearchWordDataStructure.java
+++ Trie/AddAndSearchWordDataStructure.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class AddAndSearchWordDataStructure{
     public class TrieNode{
@@ -33,7 +34,7 @@ public class AddAndSearchWordDataStructure{
                 node = children.get(c);
             }else{
                 node = new TrieNode(c);
-                children.put(c,t);
+                children.put(c, node);
             }
  
             children = node.children;
@@ -41,6 +42,43 @@ public class AddAndSearchWordDataStructure{
             if(i == word.length()-1){
                 node.isLeaf = true;
             }
+        }
+    }
+    
+    public boolean dfsSearch(HashMap<Character, TrieNode> children, String word, int start) {
+        
+        if(start == word.length()){
+            if(children.size()==0) 
+                return true; 
+            else
+                return false;
+        }
+ 
+        char c = word.charAt(start);    
+ 
+        if(children.containsKey(c)){
+            if(start == word.length()-1 && children.get(c).isLeaf){
+                return true;
+            }
+ 
+            return dfsSearch(children.get(c).children, word, start+1);
+        }else if(c == '.'){
+            boolean result = false;
+            
+            for(Map.Entry<Character, TrieNode> child: children.entrySet()){
+                if(start == word.length()-1 && child.getValue().isLeaf){
+                    return true;
+                } 
+ 
+                //if any path is true, set result to be true; 
+                if(dfsSearch(child.getValue().children, word, start+1)){
+                    result = true;
+                }
+            }
+ 
+            return result;
+        }else{
+            return false;
         }
     }
 
